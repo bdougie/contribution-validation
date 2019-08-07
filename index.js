4ad52634d319b5e2521d81c4d2c53acf4817ab20
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-08-07 12:03:53 -0700

    WIP - test the response

--- index.js
+++ index.js
@@ -2,5 +2,25 @@ const { Toolkit } = require('actions-toolkit')
 
 // Run your GitHub Action!
 Toolkit.run(async tools => {
+
+  try {
+    const pullRequest = tools.context.payload
+
+    tools.log.success(pullRequest)
+    tools.exit.success("Action is complete")
+  } catch (err) {
+    // Log the error message
+    tools.log.error(`An error occurred while looking at the pr`)
+    tools.log.error(err)
+
+    // The error might have more details
+    if (err.errors) tools.log.error(err.errors)
+
+    // Exit with a failing status
+    tools.exit.failure()
+  }
+
   tools.exit.success('We did it!')
+}, {
+  secrets: ['GITHUB_TOKEN']
 })
